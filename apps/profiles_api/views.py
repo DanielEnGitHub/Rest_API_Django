9f9e3bfd2a37804f9a895c668eaeabbcbcbe4d33
Daniel <danieljgallina@gmail.com>
2022-02-15 16:58:52 -0600

    Configurando las views y urls

--- apps/profiles_api/views.py
+++ apps/profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """ API View de prueba """
+    
+    def get(self, request, format=None):
+        """ Retornar lista de caracteristicas del APIView"""
+
+        list = [
+            'Usamos metodos HTTP como funciones (get, post, patch, delete, put)',
+            'Es similar a una vista tradicional de django',
+            'Nos da mayor control sobre la logica de nuestra aplicacion',
+            'Esta mapeando manualmente las urls'
+        ]
+
+        return Response({'message': 'hola mundo', 'list': list})
